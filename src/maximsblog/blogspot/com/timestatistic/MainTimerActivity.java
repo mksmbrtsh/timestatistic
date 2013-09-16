55250ce6dfd7827964de439b47a6f975bba35fbd
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-09-16 09:44:41 +0400

    create project

---
+++ src/maximsblog/blogspot/com/timestatistic/MainTimerActivity.java
@@ -0,0 +1,22 @@
+package maximsblog.blogspot.com.timestatistic;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainTimerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_timer);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_timer, menu);
+		return true;
+	}
+
+}
