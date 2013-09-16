022c5d63c8f8cfeaa2714b954984468beb08eeab
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-09-16 10:48:34 +0400

    add tabs and sherlock bars

--- src/maximsblog/blogspot/com/timestatistic/MainTimerActivity.java
+++ src/maximsblog/blogspot/com/timestatistic/MainTimerActivity.java
@@ -1,21 +1,60 @@
 package maximsblog.blogspot.com.timestatistic;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.viewpagerindicator.TabPageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import com.actionbarsherlock.view.Menu;
 
-public class MainTimerActivity extends Activity {
+public class MainTimerActivity extends SherlockFragmentActivity   {
 
+	private static final String[] CONTENT = new String[] { "Recent", "Artists", "Albums", "Songs", "Playlists", "Genres" };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_timer);
+		FragmentPagerAdapter adapter = new GoogleMusicAdapter(getSupportFragmentManager());
+
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
 	}
+
+    class GoogleMusicAdapter extends FragmentPagerAdapter {
+        public GoogleMusicAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return CONTENT[position % CONTENT.length].toUpperCase();
+        }
+
+        @Override
+        public int getCount() {
+          return CONTENT.length;
+        }
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_timer, menu);
+		getSupportMenuInflater().inflate(R.menu.main_timer, menu);
 		return true;
 	}
 
