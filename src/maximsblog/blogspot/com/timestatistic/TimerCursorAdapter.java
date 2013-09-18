664dbe4d0bd87350fb2e40814f1b0b92407292e3
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-09-18 11:03:50 +0400

    add start-stop time, but timer is work

--- src/maximsblog/blogspot/com/timestatistic/TimerCursorAdapter.java
+++ src/maximsblog/blogspot/com/timestatistic/TimerCursorAdapter.java
@@ -12,41 +12,56 @@ import android.widget.TextView;
 public class TimerCursorAdapter extends SimpleCursorAdapter {
 
 	private long mID;
-	
+
 	public TimerCursorAdapter(Context context, int layout, Cursor c,
 			String[] from, int[] to, int flags) {
 		super(context, layout, c, from, to, flags);
+		mID = -1;
 	}
-	
+
 	public void setCurrentTimerId(long mCurrentTimerId) {
 		mID = mCurrentTimerId;
 	}
+
 	public long getCurrentTimerId() {
 		return mID;
 	}
-	
+
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
 		super.bindView(view, context, cursor);
 		int id = cursor.getInt(0);
-		if(mID == id)
-		{
-			Cursor timeCursor = context.getContentResolver().query(RecordsDbHelper.CONTENT_URI_TIMES, new String[] { RecordsDbHelper.TIMERSID, RecordsDbHelper.STARTTIME, RecordsDbHelper.LENGHT }, RecordsDbHelper.LENGHT + " IS NULL", null , null);
-			timeCursor.moveToFirst();
-			long start = timeCursor.getLong(1);
-			long now = new Date().getTime();
-			timeCursor.close();
-			TextView t = (TextView) view.findViewById(R.id.current);
-			t.setText("+" + String.valueOf(now - start));
-		} else {
-			Cursor timeCursor = context.getContentResolver().query(RecordsDbHelper.CONTENT_URI_TIMES, new String[] { RecordsDbHelper.TIMERSID, RecordsDbHelper.STARTTIME, RecordsDbHelper.LENGHT }, RecordsDbHelper.TIMERSID+"=?", new String[]{String.valueOf(id)}, null);
-			long lenght = 0;
-			if(timeCursor.moveToLast())
-				lenght = timeCursor.getLong(2);
-			timeCursor.close();
-			TextView t = (TextView) view.findViewById(R.id.current);
-			t.setText(mContext.getString(R.string.last_value)+":"+ String.valueOf(lenght));
+		if (mID == id) {
+			Cursor timeCursor = context.getContentResolver()
+					.query(RecordsDbHelper.CONTENT_URI_TIMES,
+							new String[] { RecordsDbHelper.TIMERSID,
+									RecordsDbHelper.STARTTIME,
+									RecordsDbHelper.LENGHT },
+							RecordsDbHelper.LENGHT + " IS NULL", null, null);
+			if (timeCursor.getCount() == 1) {
+				timeCursor.moveToFirst();
+				long start = timeCursor.getLong(1);
+				long now = new Date().getTime();
+				timeCursor.close();
+				TextView t = (TextView) view.findViewById(R.id.current);
+				t.setText("+" + String.valueOf(now - start));
+				return;
+			}
 		}
+		Cursor timeCursor = context.getContentResolver().query(
+				RecordsDbHelper.CONTENT_URI_TIMES,
+				new String[] { RecordsDbHelper.TIMERSID,
+						RecordsDbHelper.STARTTIME, RecordsDbHelper.LENGHT },
+				RecordsDbHelper.TIMERSID + "=?",
+				new String[] { String.valueOf(id) }, null);
+		long lenght = 0;
+		if (timeCursor.moveToLast())
+			lenght = timeCursor.getLong(2);
+		timeCursor.close();
+		TextView t = (TextView) view.findViewById(R.id.current);
+		t.setText(mContext.getString(R.string.last_value) + ":"
+				+ String.valueOf(lenght));
+
 	}
 
 }
--- src/maximsblog/blogspot/com/timestatistic/TimersFragment.java
+++ src/maximsblog/blogspot/com/timestatistic/TimersFragment.java
@@ -1,4 +1,5 @@
 package maximsblog.blogspot.com.timestatistic;
+
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -25,125 +26,148 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public final class TimersFragment extends Fragment implements LoaderCallbacks<Cursor>, OnItemClickListener, OnClickListener {
-    private static final String KEY_CONTENT = "TestFragment:Content";
+public final class TimersFragment extends Fragment implements
+		LoaderCallbacks<Cursor>, OnItemClickListener, OnClickListener {
+	private static final String KEY_CONTENT = "TestFragment:Content";
 
-    private Timer timer;
+	private Timer timer;
 	private boolean flag_scroll;
 	private long mCurrentTimerId;
-    
-    
-    public static TimersFragment newInstance(String content) {
-    	TimersFragment fragment = new TimersFragment();
 
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < 20; i++) {
-            builder.append(content).append(" ");
-        }
-        builder.deleteCharAt(builder.length() - 1);
-        fragment.mContent = builder.toString();
+	public static TimersFragment newInstance(String content) {
+		TimersFragment fragment = new TimersFragment();
 
-        return fragment;
-    }
-    
-    TimerCursorAdapter mAdapter; 		
-    LoaderManager loadermanager;		
-    CursorLoader cursorLoader;
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < 20; i++) {
+			builder.append(content).append(" ");
+		}
+		builder.deleteCharAt(builder.length() - 1);
+		fragment.mContent = builder.toString();
 
-    private String mContent = "???";
+		return fragment;
+	}
+
+	TimerCursorAdapter mAdapter;
+	LoaderManager loadermanager;
+	CursorLoader cursorLoader;
+
+	private String mContent = "???";
 	private ListView mList;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        if ((savedInstanceState != null) && savedInstanceState.containsKey(KEY_CONTENT)) {
-            mContent = savedInstanceState.getString(KEY_CONTENT);
-        }
-        
-        loadermanager=getLoaderManager();
-    	
-    	String[] uiBindFrom = {  RecordsDbHelper.NAME };		
-    	int[] uiBindTo = {R.id.name};
-    	
-    	mAdapter = new TimerCursorAdapter(this.getActivity(), R.layout.timer_row, null, uiBindFrom, uiBindTo,0);  
-        loadermanager.initLoader(1, null, this);
-    }
+		if ((savedInstanceState != null)
+				&& savedInstanceState.containsKey(KEY_CONTENT)) {
+			mContent = savedInstanceState.getString(KEY_CONTENT);
+		}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.fragment_timers, container, false);
-        mList = (ListView)layout.findViewById(R.id.listView1);
-        Button add = new Button(getActivity());
-        add.setText("+");
-        add.setOnClickListener(this);
-        mList.addFooterView(add);
-        mList.setAdapter(mAdapter);
-        mList.setOnItemClickListener(this);
-        timerAlert();
-        return layout;
-    }
+		loadermanager = getLoaderManager();
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(KEY_CONTENT, mContent);
-    }
+		String[] uiBindFrom = { RecordsDbHelper.NAME };
+		int[] uiBindTo = { R.id.name };
+
+		mAdapter = new TimerCursorAdapter(this.getActivity(),
+				R.layout.timer_row, null, uiBindFrom, uiBindTo, 0);
+		loadermanager.initLoader(1, null, this);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		LinearLayout layout = (LinearLayout) inflater.inflate(
+				R.layout.fragment_timers, container, false);
+		mList = (ListView) layout.findViewById(R.id.listView1);
+		Button add = new Button(getActivity());
+		add.setText("+");
+		add.setOnClickListener(this);
+		mList.addFooterView(add);
+		mList.setAdapter(mAdapter);
+		mList.setOnItemClickListener(this);
+		timerAlert();
+		return layout;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString(KEY_CONTENT, mContent);
+	}
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-		CursorLoader loader = new CursorLoader(
-				         this.getActivity(),
-				         RecordsDbHelper.CONTENT_URI_TIMERS, 
-				         null, 
-				         null, 
-				         null, 
-				         null);
-				   return loader;
+		CursorLoader loader = new CursorLoader(this.getActivity(),
+				RecordsDbHelper.CONTENT_URI_TIMERS, null, null, null, null);
+		return loader;
 	}
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		if(mAdapter!=null && cursor !=null)
-		{
-			mAdapter.swapCursor(cursor); //swap the new cursor in.
-			Cursor currentTimer = getActivity().getContentResolver().query(RecordsDbHelper.CONTENT_URI_TIMES,new String[]{ RecordsDbHelper.TIMERSID, RecordsDbHelper.LENGHT}, RecordsDbHelper.LENGHT + " IS NULL", null,null );
-			currentTimer.moveToFirst();
-			mCurrentTimerId = currentTimer.getLong(0);
-			mAdapter.setCurrentTimerId(mCurrentTimerId);
-			currentTimer.close();
+		if (mAdapter != null && cursor != null) {
+			mAdapter.swapCursor(cursor); // swap the new cursor in.
+			Cursor currentTimer = getActivity().getContentResolver().query(
+					RecordsDbHelper.CONTENT_URI_TIMES,
+					new String[] { RecordsDbHelper.TIMERSID,
+							RecordsDbHelper.LENGHT },
+					RecordsDbHelper.LENGHT + " IS NULL", null, null);
+			if (currentTimer.getCount() == 1) {
+				currentTimer.moveToFirst();
+				mCurrentTimerId = currentTimer.getLong(0);
+				mAdapter.setCurrentTimerId(mCurrentTimerId);
+				currentTimer.close();
+			}
 		}
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-		
+	public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+			long arg3) {
+
 		long current_id = mAdapter.getCurrentTimerId();
-		Cursor timeCursor = getActivity().getContentResolver().query(RecordsDbHelper.CONTENT_URI_TIMES, new String[] { RecordsDbHelper.ID, RecordsDbHelper.STARTTIME, RecordsDbHelper.LENGHT }, RecordsDbHelper.TIMERSID+"=? AND " + RecordsDbHelper.LENGHT + " IS NULL", new String[]{String.valueOf(current_id)}, null);
-		timeCursor.moveToFirst();
-		long start = timeCursor.getLong(1);
-		long now =  new Date().getTime();
-		long lenght = now - start;
-		int id = timeCursor.getInt(0);
+		Cursor timeCursor = getActivity().getContentResolver().query(
+				RecordsDbHelper.CONTENT_URI_TIMES,
+				new String[] { RecordsDbHelper.ID, RecordsDbHelper.STARTTIME,
+						RecordsDbHelper.LENGHT },
+				RecordsDbHelper.TIMERSID + "=? AND " + RecordsDbHelper.LENGHT
+						+ " IS NULL",
+				new String[] { String.valueOf(current_id) }, null);
+		ContentValues cv;
+		int id;
+		long now = new Date().getTime();
+		boolean timerIsWorking = timeCursor.getCount() == 1;
+		if(timerIsWorking) {
+			timeCursor.moveToFirst();
+			long start = timeCursor.getLong(1);
+			long lenght = now - start;
+			id = timeCursor.getInt(0);
+			cv = new ContentValues();
+			cv.put(RecordsDbHelper.LENGHT, lenght);
+			getActivity().getContentResolver().update(
+					RecordsDbHelper.CONTENT_URI_TIMES, cv,
+					RecordsDbHelper.ID + "=?",
+					new String[] { String.valueOf(id) });
+		}
 		timeCursor.close();
-		ContentValues cv = new ContentValues();
-		cv.put(RecordsDbHelper.LENGHT, lenght);
-		getActivity().getContentResolver().update(RecordsDbHelper.CONTENT_URI_TIMES, cv,  RecordsDbHelper.ID+"=?", new String[]{String.valueOf(id)});
 		cv = new ContentValues();
 		Cursor c = mAdapter.getCursor();
 		c.moveToPosition(position);
 		id = c.getInt(0);
+		if (id == current_id && timerIsWorking) {
+			mAdapter.setCurrentTimerId(-1);
+			return;
+		}
 		cv.put(RecordsDbHelper.TIMERSID, id);
 		cv.put(RecordsDbHelper.STARTTIME, now);
-		getActivity().getContentResolver().insert(RecordsDbHelper.CONTENT_URI_TIMES, cv);
-		
-		
+		getActivity().getContentResolver().insert(
+				RecordsDbHelper.CONTENT_URI_TIMES, cv);
+
 		mAdapter.setCurrentTimerId(id);
 	}
 
@@ -151,10 +175,11 @@ public final class TimersFragment extends Fragment implements LoaderCallbacks<Cu
 	public void onClick(View v) {
 		ContentValues cv = new ContentValues();
 		cv.put(RecordsDbHelper.NAME, "aa");
-		getActivity().getContentResolver().insert(RecordsDbHelper.CONTENT_URI_TIMERS, cv);
+		getActivity().getContentResolver().insert(
+				RecordsDbHelper.CONTENT_URI_TIMERS, cv);
 		mAdapter.notifyDataSetChanged();
 	}
-	
+
 	public final void timerAlert() {
 
 		timer = new Timer();
@@ -166,11 +191,11 @@ public final class TimersFragment extends Fragment implements LoaderCallbacks<Cu
 
 					@Override
 					public void run() {
-						
-							int y = mList.getScrollY();
-							mList.invalidateViews();
-							mList.scrollBy(0, y);
-						}
+
+						int y = mList.getScrollY();
+						mList.invalidateViews();
+						mList.scrollBy(0, y);
+					}
 				};
 				if (!flag_scroll) {
 					getActivity().runOnUiThread(update);
@@ -182,7 +207,7 @@ public final class TimersFragment extends Fragment implements LoaderCallbacks<Cu
 		timer.scheduleAtFixedRate(tt, 1000, 1000);
 
 	}
-	
+
 	@Override
 	public void onStop() {
 		timer.cancel();
