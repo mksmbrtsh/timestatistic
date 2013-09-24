1b337b8fe8e87108dad321f1ae480af3831791ce
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-09-24 17:36:55 +0400

    add query to sum times

--- src/maximsblog/blogspot/com/timestatistic/DiagramFragment.java
+++ src/maximsblog/blogspot/com/timestatistic/DiagramFragment.java
@@ -7,9 +7,14 @@ import org.achartengine.model.SeriesSelection;
 import org.achartengine.renderer.DefaultRenderer;
 import org.achartengine.renderer.SimpleSeriesRenderer;
 
+import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +25,8 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class DiagramFragment extends Fragment{
+public class DiagramFragment extends Fragment  implements
+LoaderCallbacks<Cursor> {
 	
 	/** Colors to be used for the pie slices. */
 	  private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
@@ -35,7 +41,15 @@ public class DiagramFragment extends Fragment{
 	  /** The chart view that displays the data. */
 	  private GraphicalView mChartView;
 	private View mLayout;
-
+	private LoaderManager loadermanager;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		loadermanager = getLoaderManager();
+		loadermanager.initLoader(1, null, this);
+		super.onCreate(savedInstanceState);
+	}
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -97,11 +111,34 @@ public class DiagramFragment extends Fragment{
 	          }
 	        }
 	      });
-	      layout.addView(mChartView, new LayoutParams(LayoutParams.FILL_PARENT,
-	          LayoutParams.FILL_PARENT));
+	      layout.addView(mChartView, new LayoutParams(LayoutParams.MATCH_PARENT,
+	          LayoutParams.MATCH_PARENT));
 	    } else {
 	      mChartView.repaint();
 	    }
 	  }
+	
+	@Override
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		CursorLoader loader = new CursorLoader(this.getActivity(),
+				RecordsDbHelper.CONTENT_URI_SUMTIMES, null, null, null, null);
+		return loader;
+	}
+	
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		if (cursor != null) {
+			cursor.moveToFirst();
+			long id = cursor.getLong(0);
+			long t = cursor.getLong(1);
+			long tid = cursor.getLong(2);
+			long u = id + 1;
+		}
+	}
 
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
