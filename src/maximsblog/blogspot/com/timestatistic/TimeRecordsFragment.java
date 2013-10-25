3d942b77247366231d33c932cbfe1846a017bbb9
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-10-25 10:13:43 +0400

    add time record fragment

---
+++ src/maximsblog/blogspot/com/timestatistic/TimeRecordsFragment.java
@@ -0,0 +1,12 @@
+package maximsblog.blogspot.com.timestatistic;
+
+import android.support.v4.app.Fragment;
+
+public class TimeRecordsFragment  extends Fragment {
+
+	public static TimeRecordsFragment newInstance() {
+		
+		return new TimeRecordsFragment();
+	}
+
+}
