d7e1b4a63de36f2399335994079ddc04ec9faf54
mksmbrtsh <mksmbrtsh@gmail.com>
2014-07-10 21:22:15 +0400

    fix diagram refreshing onscr ort change

--- src/maximsblog/blogspot/com/timestatistic/DiagramFragment.java
+++ src/maximsblog/blogspot/com/timestatistic/DiagramFragment.java
@@ -53,7 +53,7 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 public class DiagramFragment extends Fragment implements
 		LoaderCallbacks<Cursor>, MainFragments {
 	private final int LOADER_ID = 3;
-	
+
 	/** The main series that will include all the data. */
 	private CategorySeries mSeries = new CategorySeries("");
 	/** The main renderer for the main dataset. */
@@ -91,15 +91,17 @@ public class DiagramFragment extends Fragment implements
 				metrics);
 		mRenderer.setLegendTextSize(val);
 		mRenderer.setLabelsTextSize(val);
-		sca = new SimpleCursorAdapter(getActivity(), android.R.id.list, null, new String[] { RecordsDbHelper.NAME }, new int[] { android.R.id.text1}, 0);
+		sca = new SimpleCursorAdapter(getActivity(), android.R.id.list, null,
+				new String[] { RecordsDbHelper.NAME },
+				new int[] { android.R.id.text1 }, 0);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		mLayout = inflater.inflate(R.layout.fragment_diagram, container, false);
-		mLegendText = (TextView)mLayout.findViewById(R.id.legend);
-		mNotFoundText = (TextView)mLayout.findViewById(R.id.not_found);
+		mLegendText = (TextView) mLayout.findViewById(R.id.legend);
+		mNotFoundText = (TextView) mLayout.findViewById(R.id.not_found);
 		mDiagramLayout = mLayout.findViewById(R.id.ScrollView1);
 		mDiagramLayout.setVisibility(View.GONE);
 		return mLayout;
@@ -109,7 +111,7 @@ public class DiagramFragment extends Fragment implements
 	public void onResume() {
 		super.onResume();
 		loadermanager.initLoader(LOADER_ID, null, this);
-		
+
 	}
 
 	@Override
@@ -134,16 +136,21 @@ public class DiagramFragment extends Fragment implements
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		if (mChartView == null) {
-			ViewGroup layout = (ViewGroup) mLayout.findViewById(R.id.chart);
-			mChartView = ChartFactory.getPieChartView(getActivity(),
-					mSeries, mRenderer);
-	        int height = mLayout.getHeight() - 2
-					* ((SherlockFragmentActivity) getActivity())
-					.getSupportActionBar().getHeight();;
-	        int width = mLayout.getWidth();
-			layout.addView(mChartView, new LayoutParams(width, height));
+		if (mChartView != null) {
+			ViewGroup chartLayout = (ViewGroup) mLayout
+					.findViewById(R.id.chart);
+			chartLayout.removeView(mChartView);
+			mChartView = null;
 		}
+			ViewGroup layout = (ViewGroup) mLayout.findViewById(R.id.chart);
+			mChartView = ChartFactory.getPieChartView(getActivity(), mSeries,
+					mRenderer);
+			int height = mLayout.getHeight()
+					- 2
+					* ((SherlockFragmentActivity) getActivity())
+							.getSupportActionBar().getHeight();
+			int width = mLayout.getWidth();
+			layout.addView(mChartView, new LayoutParams(width, height));
 		mRenderer.removeAllRenderers();
 		if (cursor != null) {
 			sca.swapCursor(cursor);
@@ -157,7 +164,7 @@ public class DiagramFragment extends Fragment implements
 			long start = cursor.getLong(3);
 			long startdate = app.getStartDate(getActivity()).date;
 			long enddate = app.getEndDate(getActivity()).date;
-			if(enddate <= startdate && enddate != -1) {
+			if (enddate <= startdate && enddate != -1) {
 				mNotFoundText.setVisibility(View.VISIBLE);
 				mDiagramLayout.setVisibility(View.GONE);
 				return;
@@ -166,13 +173,13 @@ public class DiagramFragment extends Fragment implements
 				mDiagramLayout.setVisibility(View.VISIBLE);
 			}
 			long now = new Date().getTime();
-			
+
 			String s = cursor.getString(5);
 			boolean isRunning = cursor.getInt(6) == 1;
 			Double sum = 0.0;
 			if (isRunning) {
 				if (now > enddate && enddate != -1) {
-					sum = (double) t;//enddate - start;
+					sum = (double) t;// enddate - start;
 				} else {
 					if (start < startdate)
 						start = startdate;
@@ -180,15 +187,15 @@ public class DiagramFragment extends Fragment implements
 				}
 			} else {
 				sum = (double) t;
-				/*if(start + t < enddate || enddate == -1)
-					sum = (double) t;
-				else
-					sum = (double) (enddate - start);*/
+				/*
+				 * if(start + t < enddate || enddate == -1) sum = (double) t;
+				 * else sum = (double) (enddate - start);
+				 */
 			}
 
 			SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
 			int color = cursor.getInt(7);
-			
+
 			renderer.setColor(color);
 			NumberFormat nf = NumberFormat.getPercentInstance();
 			nf.setMaximumFractionDigits(1);
@@ -206,10 +213,10 @@ public class DiagramFragment extends Fragment implements
 				s = cursor.getString(5);
 				isRunning = cursor.getInt(6) == 1;
 				double v;
-				
+
 				if (isRunning) {
 					if (now > enddate && enddate != -1) {
-						v = (double)t;//(double) enddate - start;
+						v = (double) t;// (double) enddate - start;
 					} else {
 						if (start < startdate)
 							start = startdate;
@@ -217,10 +224,10 @@ public class DiagramFragment extends Fragment implements
 					}
 				} else {
 					v = (double) t;
-					/*if(start + t < enddate || enddate == -1)
-						v = (double) t;
-					else
-						v = (double) (enddate - start);*/
+					/*
+					 * if(start + t < enddate || enddate == -1) v = (double) t;
+					 * else v = (double) (enddate - start);
+					 */
 				}
 				sum += v;
 
@@ -245,8 +252,9 @@ public class DiagramFragment extends Fragment implements
 				nvalues.add(getString(R.string.future));
 				values.add((double) enddate - now);
 				sum += enddate - now;
-			} if(enddate != -1)
-				sum = (double)(enddate - startdate);
+			}
+			if (enddate != -1)
+				sum = (double) (enddate - startdate);
 			StringBuilder sb = new StringBuilder();
 			for (int i1 = 0, cnt1 = values.size(); i1 < cnt1; i1++) {
 				sb.append("<font color=\"#");
@@ -256,7 +264,7 @@ public class DiagramFragment extends Fragment implements
 				sb.append("</font>");
 				sb.append(nvalues.get(i1));
 				sb.append(':');
-					sb.append(' ');
+				sb.append(' ');
 				sb.append(nf.format(values.get(i1) / sum));
 				sb.append(' ');
 				sb.append('(');
@@ -273,53 +281,52 @@ public class DiagramFragment extends Fragment implements
 		}
 	}
 
-	public String getTime(double time)
-	{
+	public String getTime(double time) {
 		int day;
 		int hours;
 		int minutes;
 		int seconds;
 		day = (int) (time / (24 * 60 * 60 * 1000));
 		hours = (int) (time / (60 * 60 * 1000)) - day * 24;
-		minutes = (int) (time / (60 * 1000)) - day * 24 * 60 - 60* hours;
-		seconds = (int) (time / 1000) - day * 24 * 60 * 60 - 60 * 60
-				* hours - 60 * minutes;
+		minutes = (int) (time / (60 * 1000)) - day * 24 * 60 - 60 * hours;
+		seconds = (int) (time / 1000) - day * 24 * 60 * 60 - 60 * 60 * hours
+				- 60 * minutes;
 		String s = new String();
-		if(day>0)
-		{
-			s = String.format("%s\n%02d:%02d:%02d",getTimeString("day", day), hours, minutes, seconds);
+		if (day > 0) {
+			s = String.format("%s\n%02d:%02d:%02d", getTimeString("day", day),
+					hours, minutes, seconds);
 		} else
 			s = String.format("%02d:%02d:%02d", hours, minutes, seconds);
 		return s;
 	}
-	
+
 	private String getTimeString(String res, int l) {
 		StringBuilder s = new StringBuilder();
 		s.append(l);
 		s.append(' ');
 		if (l == 1 || (l % 10 == 1 && l != 11)) {
-			s.append(getString(getResources().getIdentifier(
-					res + "1", "string", getActivity().getPackageName())));
+			s.append(getString(getResources().getIdentifier(res + "1",
+					"string", getActivity().getPackageName())));
 		} else if ((l % 10 == 2 || l % 10 == 3 || l % 10 == 4) && l != 12
 				&& l != 13 && l != 14) {
-			s.append(getString(getResources().getIdentifier(
-					res + "234", "string", getActivity().getPackageName())));
+			s.append(getString(getResources().getIdentifier(res + "234",
+					"string", getActivity().getPackageName())));
 		} else
-			s.append(getString(getResources().getIdentifier(
-					res + "s", "string", getActivity().getPackageName())));
+			s.append(getString(getResources().getIdentifier(res + "s",
+					"string", getActivity().getPackageName())));
 		return s.toString();
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> arg0) {
-		
+
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 	};
-	
+
 	@Override
 	public void onReload() {
 		loadermanager.restartLoader(LOADER_ID, null, this);
